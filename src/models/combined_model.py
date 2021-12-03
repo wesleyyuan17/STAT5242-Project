217a9a7af08c16aff28cd12623132702b7ed9dfa
Wesley Yuan <wesleyyuan@dyn-160-39-133-51.dyn.columbia.edu>
2021-12-03 10:48:40 -0500

    implemented combined model

--- src/models/combined_model.py
+++ src/models/combined_model.py
@@ -6,8 +6,33 @@ from components import *
 
 
 class GraphLSTM(BaseModel):
-    def __init__(self) -> None:
-        super().__init__()
+    def __init__(self, n_features, lstm_hidden_dim, lstm_n_layers=1, gcn_pred_per_node=1, gcn_n_layers=1, gcn_hidden_dim=1) -> None:
+        """
+        Combines LSTM and GCN models into single e2e trainable model 
 
-    def forward(self, x):
-        pass
+        Args:
+            n_features: int, number of features per node (asset)
+            lstm_hidden_dim: int, dimensionality of LSTM hidden layer(s)
+            lstm_n_layers: int, number of layers in LSTM model
+            gcn_pred_per_node: int, number of outputs each node fed into gcn is mapped to i.e. with n nodes output is size n * gcn_pred_per_node
+            gcn_n_layers: int, (not supported) number of layers in GCN model
+            gcn_hidden_dim: int, (not supported) number of outputs in hidden dim each node in input is mapped to i.e. with n nodes, hidden layer has dimension n * gcn_hidden_dim
+        """
+        super().__init__()
+        self.lstm = nn.LSTM(input_size=14*n_features, hidden_size=lstm_hidden_dim, num_layers=lstm_n_layers, batch_first=True)
+        self.gcn = GCN(n_features, gcn_pred_per_node)
+
+        self.model_weights = nn.Parameter(torch.ones(2))
+
+    def forward(self, x, adj):
+        # feed through lstm
+        lstm_output = self.lstm(x)
+
+        # feed through gcn
+        gcn_output = self.gcn(x[-1], adj) # -1 to get latest state since gcn rn takes in just one day's price data
+
+        # combine using learnable weights
+        self.model_weights /= self.model_weights.sum() # normalize to sum to 1
+        final_output = self.model_weights[0]*lstm_output + self.model_weights[1]*gcn_output
+
+        return final_output
